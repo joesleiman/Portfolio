cae9180524a02c75dcbe1af999d9b0c373406883
joesleiman <joe.c.sleiman@gmail.com>
2025-10-31 01:03:50 -0700

    replace appostrophe with &apos;

--- src/app/components/intro/Intro.tsx
+++ src/app/components/intro/Intro.tsx
@@ -6,10 +6,10 @@ export default function Intro() {
                                mb-[1.5rem] 
                                text-center
                                font-extrabold'>
-                    Hi, I'm Joe. Nice to meet you.
+                    Hi, I&apos;m Joe. Nice to meet you.
                 </h1>
                 <h2 className='leading-[1.8rem] text-center'>
-                  Since starting my journey as a frontend developer 8 years ago, I've built high-quality websites and hybrid mobile apps, shipped features on large-scale platforms like <strong>Bill.com</strong>, and created a full e-learning platform <strong>Ethrai</strong> serving over a million users. I’ve collaborated with talented teams to turn complex problems into scalable, maintainable solutions. Quietly confident, naturally curious, and always focused on crafting digital experiences that both users and developers love.
+                  Since starting my journey as a frontend developer 8 years ago, I&aposve built high-quality websites and hybrid mobile apps, shipped features on large-scale platforms like <strong>Bill.com</strong>, and created a full e-learning platform <strong>Ethrai</strong> serving over a million users. I&aposve collaborated with talented teams to turn complex problems into scalable, maintainable solutions. Quietly confident, naturally curious, and always focused on crafting digital experiences that both users and developers love.
                 </h2>
             </div>
         </section>
