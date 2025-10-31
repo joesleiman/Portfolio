0c4447b0f7cf39afa73160d94480f7264c45c9f7
joesleiman <joe.c.sleiman@gmail.com>
2025-10-31 00:49:20 -0700

    add hero and intro section

---
+++ src/app/components/intro/Intro.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import linkedinProfile from '@/app/assets/images/linkedin-profile.jpg';
+import heroDevices from '@/app/assets/images/hero-devices.svg';
+
+export default function Intro() {
+    return (
+        <section className='px-8 pt-[9rem] pb-[18rem] bg-(--color-primary) text-(--color-tertiary)'>
+            <div className='w-[60%] mx-auto'>
+                <h1 className='text-[2rem] 
+                               mb-[1.5rem] 
+                               text-center
+                               font-extrabold'>
+                    Hi, I'm Joe. Nice to meet you.
+                </h1>
+                <h2 className='leading-[1.8rem] text-center'>
+                  Since starting my journey as a frontend developer 8 years ago, I've built high-quality websites and hybrid mobile apps, shipped features on large-scale platforms like <strong>Bill.com</strong>, and created a full e-learning platform <strong>Ethrai</strong> serving over a million users. I’ve collaborated with talented teams to turn complex problems into scalable, maintainable solutions. Quietly confident, naturally curious, and always focused on crafting digital experiences that both users and developers love.
+                </h2>
+            </div>
+        </section>
+    );
+}
