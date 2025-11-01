c54c4317ba335bb97be40bb044bb0e9f29b2827a
joesleiman <joe.c.sleiman@gmail.com>
2025-11-01 01:16:42 -0700

    i don't know

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+
 import Image from 'next/image';
 import linkedinProfile from '@/app/assets/images/linkedin-profile.jpg';
 import heroDevices from '@/app/assets/images/hero-devices.svg';
@@ -5,22 +6,22 @@ import heroDevices from '@/app/assets/images/hero-devices.svg';
 export default function Hero() {
     return (
         <section className='px-8'>
-            <div className='flex flex-col items-center pt-[3rem] pb-[3rem]'> 
+            <div className='flex flex-col items-center pt-[3rem] pb-[3rem]'>
                 <h1 className='text-(--color-secondary) 
                                text-[2rem] 
                                mb-[1.5rem] 
                                font-extrabold'>
-                                Senior Front-End Engineer & UI Craftsman
+                    Senior Front-End Engineer & UI Craftsman
                 </h1>
                 <h2 className='font-medium mb-[1.5rem]'>
                     I bring designs to life with clean code, thoughtful details, and a love for great user experiences
                 </h2>
-                <Image className='rounded-full w-[212px] mt-[2rem]' 
-                       src={linkedinProfile}
-                       alt='LinkedIn Profile'/>
+                <Image className='rounded-full w-[212px] mt-[2rem]'
+                    src={linkedinProfile}
+                    alt='LinkedIn Profile' />
             </div>
-            <Image src={heroDevices} alt='Hero Devices' 
-                   className='h-auto md:max-w-[860px] mx-auto'/>
+            <Image src={heroDevices} alt='Hero Devices'
+                className='h-auto md:max-w-[860px] mx-auto' />
         </section>
     );
 }
