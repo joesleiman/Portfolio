c844da215229103d720f7ad09cd2a32e554f72dc
joesleiman <joe.c.sleiman@gmail.com>
2025-11-03 18:54:40 -0800

    add recent work to project card data

--- src/app/data/projects.data.ts
+++ src/app/data/projects.data.ts
@@ -1,7 +1,8 @@
 
 import { StaticImageData } from "next/image";
-import linkedinProfile from '@/app/assets/images/linkedin-profile.jpg';
 import billLogo from '@/app/assets/images/bill-com.jpg';
+import biotechne from '@/app/assets/images/biotechne.jpg';
+import Ethrai from '@/app/assets/images/ethrai.jpg';
 
 export interface Project {
   image: StaticImageData | string;
@@ -9,20 +10,20 @@ export interface Project {
   link: string;
 }
 export const projects = [
+     {
+      image: Ethrai,
+      title: "an electronic training and knowledge platform for public employees in Saudi Arabia to develop their skills through e-learning, e-books, and interactive sessions.",
+      link: "https://apps.apple.com/us/app/ethrai/id1438205024",
+    },
     {
       image: billLogo,
       title:
         "financial platform that helps businesses pay their bills and get paid by customers more simply and efficiently.",
-      link: "https://bill.com",
+      link: "https://www.bill.com/",
     },
     {
-      image: linkedinProfile,
-      title: "Modern web app for creative professionals.",
-      link: "https://example.com",
-    },
-    {
-      image: linkedinProfile,
-      title: "AI-powered design automation platform.",
-      link: "https://example.ai",
-    },
+      image: biotechne,
+      title: "empowers researchers in Life Science and Clinical Diagnostics by providing high-quality workflow solutions.",
+      link: "https://www.bio-techne.com/reagents/rnascope-ish-technology/probes/custom/request-form",
+    }
   ];
